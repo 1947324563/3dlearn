ea4f55f0cfff020bf3da19e2e76e14679866096b
火箭 <1947324563@qq.com>
2019-07-16 23:19:23 +0800

    vue代码删除

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Three from 'three'
 
 Vue.config.productionTip = false
 
+// @ts-ignore
 new Vue({
   router,
   store,
+  Three,
   render: h => h(App)
 }).$mount('#app')
